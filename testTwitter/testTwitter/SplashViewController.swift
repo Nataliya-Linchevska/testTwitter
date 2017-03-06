f08aeea731d7a58564d49eab10cc9f8571724192
Kejnnary <kejnnary@mail.ru>
2017-03-06 09:49:29 +0200

    Init. Login screen

---
+++ testTwitter/testTwitter/SplashViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SplashViewController.swift
+//  testTwitter
+//
+//  Created by user on 06.03.17.
+//  Copyright © 2017 GeekHub. All rights reserved.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
